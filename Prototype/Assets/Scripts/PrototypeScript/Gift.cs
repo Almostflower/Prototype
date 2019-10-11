ac244888d58f8201af59c9d0e9bc40342eb1ee51
na_cho <ryoya.19970625@gmail.com>
2019-10-11 11:19:21 +0900

    ギフト　作成途中

---
+++ Prototype/Assets/Scripts/PrototypeScript/Gift.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gift : BaseMonoBehaviour
+{
+    /// <summary>
+    /// ギフトのprefab
+    /// </summary>
+    [SerializeField] private GameObject giftData;
+
+    /// <summary>
+    /// ギフトの寿命
+    /// </summary>
+    [SerializeField]private float time;
+
+    /// <summary>
+    /// 経過タイム
+    /// </summary>
+    private float progressTime;
+
+    /// <summary>
+    /// ギフトの生存確認
+    /// </summary>
+    private bool isDead;
+    public bool IsDead
+    {
+        get { return isDead; }
+    }
+
+
+    /// <summary>
+    /// ギフトの初期化
+    /// </summary>
+    protected override void Awake()
+    {
+        base.Awake();
+
+        progressTime = 0;
+        isDead = false;
+    }
+
+    /// <summary>
+    /// ギフトの更新
+    /// </summary>
+    public override void UpdateNormal()
+    {
+        if (isDead) { return; }
+
+        progressTime += Time.deltaTime;
+
+        if(progressTime >= 1)
+        {
+            time--;
+            progressTime = 0;
+        }
+
+        if(time <= 0)
+        {
+            isDead = true;
+        }
+    }
+}
