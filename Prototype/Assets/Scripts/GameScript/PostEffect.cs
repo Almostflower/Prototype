0c2bf74adf8f2c1531fe9f44dbd08ed4bfc2a7db
na_cho <ryoya.19970625@gmail.com>
2019-11-15 11:46:36 +0900

    ステージマネージャー変更

---
+++ Prototype/Assets/Scripts/GameScript/PostEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostEffect : MonoBehaviour
+{
+    public Material outline;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        // ポストエフェクトをかける
+        Graphics.Blit(source, destination, outline);
+    }
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    
+    //}
+    //
+    //// Update is called once per frame
+    //public override void UpdateNormal()
+    //{
+    //    
+    //}
+}
