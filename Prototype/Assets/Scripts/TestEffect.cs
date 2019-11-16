e382f1a542111def00a53dbf21cb990bd0528dd4
na_cho <ryoya.19970625@gmail.com>
2019-11-16 16:08:37 +0900

    エフェクト用シーン追加（テスト）

---
+++ Prototype/Assets/Scripts/TestEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEffect : MonoBehaviour
+{
+    /// <summary>
+    /// パーティクルで使用するパーツ
+    /// </summary>
+    [SerializeField, Tooltip("使用するオブジェクト")] private GameObject[] objs;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
