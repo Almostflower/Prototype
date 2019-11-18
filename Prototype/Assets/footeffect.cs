c4b8f33c5be65e9811cdbfa5158dcb1e75a0173c
MandaiRyuta <Mandai1990@outlook.jp>
2019-11-18 23:01:05 +0900

    キャラクター待機モーション追加

---
+++ Prototype/Assets/footeffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footeffect : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem ps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(ps, 3.0f);
+    }
+}
