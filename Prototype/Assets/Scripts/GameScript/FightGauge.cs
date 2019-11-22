196fd827f43df103cdd1664d1984563db82b3e6d
na_cho <ryoya.19970625@gmail.com>
2019-11-22 14:05:35 +0900

    握力ゲージ追加

--- Prototype/Assets/Scripts/GameScript/FightGauge.cs
+++ Prototype/Assets/Scripts/GameScript/FightGauge.cs
@@ -39,13 +39,11 @@ public class FightGauge : BaseMonoBehaviour
 	public override void UpdateNormal()
 	{
 		// うさぎを持ったら
-		//if (player_.HoldingRabbitFlag)
-		if (Input.GetKey(KeyCode.Space))
+		if (player_.HoldingRabbitFlag)
 		{
 			background_.enabled = true;
 			count_ = 0;
-			//background_.fillAmount = player_.HoldingTimeCounter / player_.HoldingTime;
-			background_.fillAmount -= 0.1f;
+			background_.fillAmount = player_.HoldingTimeCounter / player_.HoldingTime;
 		}
 		else
 		{
