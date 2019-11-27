25f79d60619559d46adf571d9284d272ac147dd5
MandaiRyuta <tjddydxo2002@gmail.com>
2019-11-27 17:41:21 +0900

    チュートリアルのスクリプト＆プレイヤーダッシュ編集

--- Prototype/Assets/Scripts/GameScript/Player.cs
+++ Prototype/Assets/Scripts/GameScript/Player.cs
@@ -185,6 +185,36 @@ public sealed class Player : BaseMonoBehaviour
         this.transform.GetChild(6).GetChild(0).gameObject.GetComponent<Text>().text = "良いGiftの数：" + goodGiftNum.ToString();
         this.transform.GetChild(6).GetChild(1).gameObject.GetComponent<Text>().text = "悪いGiftの数：" + badGiftNum.ToString();
     }
+
+    /// <summary>
+    /// 現在の回転方向のベクトル
+    /// </summary>
+    private Vector3 currentRotateForward = Vector3.zero;
+
+    /// <summary>
+    /// キャラクターの回転を行う
+    /// </summary>
+    protected void DoRotate()
+    {
+        // 現在の移動ベクトルを取得
+        Vector3 moveVelocity = PlayerController.velocity;
+        moveVelocity.y = 0;
+
+        // 移動ベクトルが零ベクトル以外の場合は回転用ベクトルに設定
+        if (moveVelocity != Vector3.zero)
+        {
+            currentRotateForward = moveVelocity;
+        }
+
+        // 角度と回転方向を取得
+        float value = Mathf.Min(1, Speed * Time.deltaTime / Vector3.Angle(transform.forward, currentRotateForward));
+        Vector3 newForward = Vector3.Slerp(transform.forward, currentRotateForward, value);
+
+        if (newForward != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(newForward, transform.up);
+        }
+    }
     private void PlayerMove()
     {
         //velocity = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
@@ -206,30 +236,47 @@ public sealed class Player : BaseMonoBehaviour
 
         if (PlayerController.isGrounded)
         {
-            Direction = (transform.forward * Input.GetAxis("Vertical")) * Speed * Time.fixedDeltaTime;
             transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed * Time.fixedDeltaTime, 0);
-            if (Input.GetKeyDown(KeyCode.LeftShift))
+
+            if (Input.GetAxis("Vertical") >= 0.0f)
             {
-                //走るアニメーション速度変更
-                PlayerAnimator.SetFloat("Speed", 1.5f);
-                SparkParticle.SetActive(true);
-                speedflag = true;//加速時
+                Direction = (transform.forward * Input.GetAxis("Vertical")) * Speed * Time.fixedDeltaTime;
+
+                if (Input.GetKeyDown(KeyCode.LeftShift))
+                {
+                    //走るアニメーション速度変更
+                    PlayerAnimator.SetFloat("Speed", 1.5f);
+                    SparkParticle.SetActive(true);
+                    speedflag = true;//加速時
+                }
+                else if (Input.GetKeyUp(KeyCode.LeftShift))
+                {
+                    //走るアニメーション速度変更
+                    PlayerAnimator.SetFloat("Speed", 0.8f);
+                    SparkParticle.SetActive(false);
+                    speedflag = false;//通常時
+                }
+
+                if (Input.GetAxis("Vertical") != 0f)
+                {
+                    PlayerAnimator.SetFloat(PlayerActionParameter, 1f);
+                }
+
+                if (Input.GetAxis("Horizontal") >= 0f)
+                {
+                    PlayerAnimator.SetFloat(PlayerActionParameter, 1f);
+                }
             }
-            else if(Input.GetKeyUp(KeyCode.LeftShift))
+            else
             {
                 //走るアニメーション速度変更
                 PlayerAnimator.SetFloat("Speed", 0.8f);
                 SparkParticle.SetActive(false);
                 speedflag = false;//通常時
-            }
-            if (Input.GetAxis("Vertical") != 0f)
-            {
-                PlayerAnimator.SetFloat(PlayerActionParameter, velocity.magnitude);
-            }
 
-            if (Input.GetAxis("Horizontal") != 0f)
-            {
-                PlayerAnimator.SetFloat(PlayerActionParameter, velocity.magnitude);
+                Direction = (transform.forward * Input.GetAxis("Vertical")) * (Speed * 0.6f) * Time.fixedDeltaTime;
+                PlayerAnimator.SetFloat("Move", 0.3f);
+                PlayerAnimator.SetFloat(PlayerActionParameter, 0.6f);
             }
 
             if (Input.GetAxis("Vertical") == 0f && Input.GetAxis("Horizontal") == 0f)
