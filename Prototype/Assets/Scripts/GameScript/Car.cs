e3693850fd2ffd2366bf7e4a69fa8450b8345912
na_cho <ryoya.19970625@gmail.com>
2019-11-27 14:35:42 +0900

    車の処理一時中断

---
+++ Prototype/Assets/Scripts/GameScript/Car.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : BaseMonoBehaviour
+{
+
+    private Vector3 StartPos;
+    private Vector3 EndPos;
+
+    /// <summary>
+    /// BaseMonoVehavior初期化
+    /// </summary>
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    void Start()
+    {
+
+    }
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    public override void UpdateNormal()
+    {
+
+    }
+
+}
