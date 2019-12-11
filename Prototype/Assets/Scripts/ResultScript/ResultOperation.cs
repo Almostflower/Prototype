463003931b7dfe5cf781fb83ab2fb9922c206c99
MandaiRyuta <tjddydxo2002@gmail.com>
2019-12-11 16:46:13 +0900

    リザルトの処理追加

---
+++ Prototype/Assets/Scripts/ResultScript/ResultOperation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+public class ResultOperation : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+
+    private float Wmargin = 0.0f;
+    //private float Hmargin = 1.0f;
+    private void Awake()
+    {
+        cam.rect = new Rect(0.0f, 0.0f, Wmargin, 1.0f);
+    }
+    private void Update()
+    {
+        if(Wmargin < 1.0f)
+        {
+            Wmargin += 0.01f;
+        }
+        //if(Hmargin > 0.0f)
+        //{
+        //    Hmargin -= 0.01f;
+        //}
+        cam.rect = new Rect(0.0f, 0.0f, Wmargin, 1.0f);
+    }
+}
---
+++ Prototype/Assets/Scripts/ResultScript/ResultPlayer.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private Animator anim;
+
+    private bool ActionAnimFlag;
+    private int Actionnum;
+    public bool ActionAnim
+    {
+        get { return ActionAnimFlag; }
+        set { ActionAnimFlag = value; }
+    }
+
+    public int ActionNumber
+    {
+        get { return Actionnum; }
+        set { Actionnum = value; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(gameObject.transform.rotation.y);
+        gameObject.transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f), 90.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //220
+        if(gameObject.transform.position.x > 418.4)
+        {
+            anim.SetBool("Walk", true);
+            gameObject.transform.position += new Vector3(-0.1f, 0.0f, 0.0f);
+        }
+        else
+        {
+            //正面向く
+            //if (gameObject.transform.rotation.y <= 0.99f)
+            //{
+            //    Debug.Log(gameObject.transform.rotation.y);
+            //    gameObject.transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f), -1.0f);
+            //}
+            //else
+            //{
+            //    anim.SetBool("Walk", false);
+            //    anim.SetBool("Wait", true);
+            //}
+            //カメラのほう向く
+            if(gameObject.transform.rotation.y <= 0.93f)
+            {
+                gameObject.transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f), -1.0f);
+            }
+            else
+            {
+                anim.SetBool("Walk", false);
+                anim.SetBool("Wait", true);
+            }
+        }
+
+        //スコアに対するアクション
+        //if(ActionAnim)
+        //{
+        //    switch(ActionNumber)
+        //    {
+        //        case 0:
+        //            anim.SetBool("Wait", true);
+        //            break;
+        //        case 1:
+        //            anim.SetBool("Wait", true);
+        //            break;
+        //        case 2:
+        //            anim.SetBool("Wait", true);
+        //            break;
+        //        default:
+        //            break;
+        //    }
+        //}
+    }
+}
