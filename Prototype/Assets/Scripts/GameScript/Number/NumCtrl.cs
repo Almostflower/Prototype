b9edb3640a09f6e4ea6ecf27854ac0b78fe24fbe
Okada Kanehira <okada.kanehira.as@gmail.com>
2019-12-17 17:30:46 +0900

    UI数字のスクリプトを変更した。

---
+++ Prototype/Assets/Scripts/GameScript/Number/NumCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumCtrl : MonoBehaviour
+{
+	[SerializeField] private Sprite[] sp = new Sprite[10];
+
+	public void ChangeSprite(int no)
+	{
+		if (no > 9 || no < 0) no = 0;
+
+		//SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		//spriteRenderer.sprite = sp[no];
+
+		Image image_no = gameObject.GetComponent<Image>();
+		image_no.sprite = sp[no];
+	}
+}
